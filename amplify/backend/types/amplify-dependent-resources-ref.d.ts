44da065f2b54f06d489561c4d3bdbcfa093860ee
aksel93 <alrogers1993@outlook.com>
2023-02-08 12:51:17 -0600

    build successfully damnit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "crank": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
